21c722fd5520834771727f0f833222a9f5fe8c8b
meramkumar <meramkumar@gmail.com>
2020-04-25 23:23:27 -0500

    Updated the master page with style transition effect on menu

--- sample-shopping/src/components/menu-item/menu-item.component.jsx
+++ sample-shopping/src/components/menu-item/menu-item.component.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './menu-item.style.scss';
 
-const MenuItem = ({title, imageUrl}) =>
+const MenuItem = ({title, imageUrl, size}) =>
 (
-    <div style={{
+    <div className={`${size} menu-item`}>
+    <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
-    }} className='menu-item'>
+     }} ></div>
+
     <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
     </div>
 </div> 
