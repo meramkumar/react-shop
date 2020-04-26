fc8c0f2d7f7c562be6295c5f0537f00385aeaafe
meramkumar <meramkumar@gmail.com>
2020-04-26 13:21:34 -0500

    Router added

--- sample-shopping/src/index.js
+++ sample-shopping/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 //import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import {BrowserRouter} from 'react-router-dom'; 
+
+ReactDOM.render( 
+  <BrowserRouter>
+  <App /> 
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
