ad000b419212b3df501db2ce5601cb33c78072d3
meramkumar <meramkumar@gmail.com>
2020-05-08 09:59:24 -0500

    Custom Button Component added

---
+++ src/components/form-button/form-button.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './form-button.style.scss';
+
+
+const FormButton = ({children, ...otherProps}) =>
+(
+   <button className='custom-button' {...otherProps}>
+
+    {children}
+   </button>
+)
+
+export default FormButton;
